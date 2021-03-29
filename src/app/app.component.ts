a73f6bfbc392f23c5b9c21649ee89188b5bb888a
marcusungarette <marcusungarette@gmail.com>
2021-03-29 20:49:15 -0300

    Added Input field

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'pipes-payment';
+  name: string;
+
+  onNameChange(value: string) {
+    this.name = value;
+  }
 }
