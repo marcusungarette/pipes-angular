8d3d7dd40b78ac0113950021158234751781d05d
marcusungarette <marcusungarette@gmail.com>
2021-03-29 23:41:12 -0300

    Added more options to pipe - meters and centimeters option

--- src/app/convert-miles.pipe.ts
+++ src/app/convert-miles.pipe.ts
@@ -4,10 +4,20 @@ import { Pipe, PipeTransform } from '@angular/core';
   name: 'convertMiles',
 })
 export class ConvertMilesPipe implements PipeTransform {
-  transform(value: any, ...args: any[]): unknown {
+  transform(value: any, targetUnits: string): any {
     if (!value) {
       return '';
     }
-    return value * 1.60934;
+
+    switch (targetUnits) {
+      case 'km':
+        return value * 1.60934;
+      case 'm':
+        return value * 1.60934 * 1000;
+      case 'cm':
+        return value * 1.60934 * 1000 * 1000;
+      default:
+        throw new Error('Target units does not supported');
+    }
   }
 }
